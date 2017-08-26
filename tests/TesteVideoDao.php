4b0b6f885fb4bf510cfd44181f4ab3d8c33cdfd0
Marcio Lucas <marciioluucas@gmail.com>
2017-08-26 16:49:43 -0300

    Colocando o JWT

---
+++ tests/TesteVideoDao.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marci
+ * Date: 26/08/2017
+ * Time: 16:33
+ */
+
+namespace tests;
+
+
+
+use PHPUnit\Framework\TestCase;
+use dao\VideoDAO;
+use model\Video;
+
+final class TesteVideoDao extends TestCase
+{
+    private $video;
+
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertInstanceOf(
+            VideoDAO::class,
+            VideoDAO::retreave($this->video)
+        );
+    }
+
+    public function testarRetreaveDinamico()
+    {
+        $this->video = new Video();
+        $this->video->setNome("Um dia de verão");
+        $this->video->setGenero("Comédia");
+
+        $this->assertEquals(
+            "SELECT * FROM video  WHERE (ativado = :condition_ativado AND
+ (nome LIKE CONCAT('%',:condition_nome,'%') AND genero = :condition_genero)", VideoDAO::retreave($this->video));
+    }
+}
