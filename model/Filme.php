0fe06ef74b81a09020a67793ca6323459b8b5b02
Marcio Lucas <marciioluucas@gmail.com>
2017-08-26 17:08:17 -0300

    Abstraindo video e adicionando filme e serie.

---
+++ model/Filme.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marci
+ * Date: 26/08/2017
+ * Time: 17:01
+ */
+
+namespace model;
+
+
+class Filme extends Video
+{
+
+}
---
+++ model/Serie.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marci
+ * Date: 26/08/2017
+ * Time: 17:01
+ */
+
+namespace model;
+
+
+class Serie extends Video
+{
+    private $episodios;
+    private $temporadas;
+
+    /**
+     * @return mixed
+     */
+    public function getEpisodios()
+    {
+        return $this->episodios;
+    }
+
+    /**
+     * @param mixed $episodios
+     */
+    public function setEpisodios($episodios)
+    {
+        $this->episodios = $episodios;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTemporadas()
+    {
+        return $this->temporadas;
+    }
+
+    /**
+     * @param mixed $temporadas
+     */
+    public function setTemporadas($temporadas)
+    {
+        $this->temporadas = $temporadas;
+    }
+
+
+}
