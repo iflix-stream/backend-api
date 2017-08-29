1c02316eb9f33fa2ea4bb97a8a9d1f9857bd588e
Marcio Lucas <marciioluucas@gmail.com>
2017-08-29 14:40:44 -0300

    Adicionando TODO e classe Mensagem.

---
+++ util/Mensagem.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marci
+ * Date: 29/08/2017
+ * Time: 14:39
+ */
+
+namespace util;
+
+
+class Mensagem
+{
+//TODO: Fazer classe pra retornar os erros via array
+}
