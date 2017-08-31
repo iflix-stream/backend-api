0740f626f20be83b0424ba13b6e11e33ef504364
JuanesAdriano <juaneshtk50@gmail.com>
2017-08-31 13:12:33 -0300

    Criando atributos e criando métodos(TO DO) na classe MinhaLista.php

--- model/MinhaLista.php
+++ model/MinhaLista.php
@@ -12,4 +12,48 @@ namespace model;
 class MinhaLista
 {
 
+    private $idPerfil;
+    private $itens;
+
+    /**
+     * @return mixed
+     */
+    public function getIdPerfil()
+    {
+        return $this->idPerfil;
+    }
+
+    /**
+     * @param mixed $idPerfil
+     */
+    public function setIdPerfil($idPerfil)
+    {
+        $this->idPerfil = $idPerfil;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getItens()
+    {
+        return $this->itens;
+    }
+
+    /**
+     * @param mixed $itens
+     */
+    public function setItens($itens)
+    {
+        $this->itens = $itens;
+    }
+
+
+
+    public function adicionarItem(){
+        //TODO: método de adiconar itens na lista;
+    }
+    public function removerItem(){
+        //TODO: Método de remover itens da Lista;
+    }
+
 }
