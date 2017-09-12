06b5eb1423cd280179d6981934983763924ef24d
Marcio Lucas <marciioluucas@gmail.com>
2017-09-12 11:17:30 -0300

    Mudando coisas cadastro filme

--- model/Video.php
+++ model/Video.php
@@ -157,7 +157,8 @@ class Video extends MediaFactory
 
     public function cadastrar()
     {
-
+        //Cadastrar e colocar o nome do vídeo para o ID, não esquecer de colocar subdiretorio filme/serie
+        //Se caso não fazer o upload, deletar o filme/serie cadastrado do banco.
         $data = new DataConversor();
         $data = $data->converter();
 
@@ -193,11 +194,11 @@ class Video extends MediaFactory
     public function stream()
     {
 
-        if(empty($this->id)){
+        if (empty($this->id)) {
 //            echo (new Mensagem())->error("parametro-id-nulo",500);
             die("Parametro ID nulo");
         }
-        $path = dirname(__FILE__) . "/../video/".$this->id.".mp4";
+        $path = dirname(__FILE__) . "/../video/" . $this->id . ".mp4";
 
         $finfo = new finfo(FILEINFO_MIME);
         $mime = $finfo->file($path);
@@ -253,9 +254,10 @@ class Video extends MediaFactory
         }
     }
 
-    public function retreaveLista(){
+    public function retreaveLista()
+    {
 
-       return VideoDAO::newRetreaveLista();
+        return VideoDAO::newRetreaveLista();
     }
 
     public function listarRecomendados($idUsuario)
@@ -268,8 +270,10 @@ class Video extends MediaFactory
         $salvar = new Arquivo();
         return $salvar->salvar("Video", "arquivo");
     }
-    public function adicionarItemLista(){
 
-        return VideoDAO::adicionarItemLista($this->getTipo(),$this->getId());
+    public function adicionarItemLista()
+    {
+
+        return VideoDAO::adicionarItemLista($this->getTipo(), $this->getId());
     }
 }
