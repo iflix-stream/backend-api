e6e155feeaabaa65582dafef0a5b722921718ac0
Marcio Lucas <marciioluucas@gmail.com>
2017-09-14 22:27:02 -0300

    Funcionando retreave de série com Temporadas e episódios.

--- controller/FilmeController.php
+++ controller/FilmeController.php
@@ -43,12 +43,12 @@ class FilmeController implements IController
         if (isset($params['id'])) $filme->setId($params['id']);
         if (isset($params['nome'])) $filme->setNome($params['nome']);
         if (isset($params['genero'])) $filme->setGenero($params['genero']);
-        $data = ["SQL" => "" . $filme->listar() . ""];
+        if (!isset($_GET['stream'])) View::render($filme->listar());
         if ($_GET['stream'] == "true") {
             $filme->setId($_GET['id']);
             $filme->stream();
         }
-        View::render($data);
+
     }
 
     public function put($params = [])
